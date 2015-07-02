71802e93a2f7a4b7ad1b98edb8963a939ea1d63c
MarkClaessens <mark-claessens@hotmail.com>
2015-07-02 18:48:16 +0200

    unittest

---
+++ ______Reparatie/Reservering_Reparatie/unittestRESERVERING/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Reservering_Reparatie;
+namespace unittestRESERVERING
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Boeking boeking = new Boeking();
+            boeking.LaatsteHoofdbezoeker("jan");
+        }
+    }
+}
