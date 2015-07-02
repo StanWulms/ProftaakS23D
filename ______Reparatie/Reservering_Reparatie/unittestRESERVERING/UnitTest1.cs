83b55161ffb38f1d9faf6e0c6bd5ed4974e274b5
MarkClaessens <mark-claessens@hotmail.com>
2015-07-02 20:45:43 +0200

    unittest

--- ______Reparatie/Reservering_Reparatie/unittestRESERVERING/UnitTest1.cs
+++ ______Reparatie/Reservering_Reparatie/unittestRESERVERING/UnitTest1.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Reservering_Reparatie;
+using Oracle.DataAccess.Client;
+using Oracle.DataAccess.Types;
 namespace unittestRESERVERING
 {
     [TestClass]
@@ -9,8 +11,18 @@ namespace unittestRESERVERING
         [TestMethod]
         public void TestMethod1()
         {
-            Boeking boeking = new Boeking();
-            boeking.LaatsteHoofdbezoeker("jan");
+            Hoofdboeker hb = new Hoofdboeker("henk", "", "Janssen", "straat", "9", "plaats", "nummer");
+            string actual = hb.ToString();
+            string expected = "henk";
+            Assert.AreEqual(expected, actual);
         }
+        public void TestMethod2()
+        {
+            Account ac = new Account("henk","email","hash");
+            string actual = ac.ToString();
+            string expected = "henk";
+            Assert.AreEqual(expected, actual);
+        }
+        
     }
 }
